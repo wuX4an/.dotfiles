efbf309006dd0e7e7c3ab9749bdc1be91d8b9d08
wux4an <wux4an@duck.com>
2025-03-10 17:49:40 -0500

    center qr to center and format

--- .config/qtile/settings/modules/network/fzf.py
+++ .config/qtile/settings/modules/network/fzf.py
@@ -4,18 +4,18 @@ import time
 from getpass import getpass
 import network
 
-def menu():
 
+def menu():
     items = network.scan()
-    dir_path = '/tmp/net/'
+    dir_path = "/tmp/net/"
     os.makedirs(dir_path, exist_ok=True)
 
     for wifi in items:
-        ssid = wifi['SSID']
+        ssid = wifi["SSID"]
         file_path = os.path.join(dir_path, f"{ssid}")
         data = f"SSID: {wifi['SSID']}\nRATE: {wifi['RATE']}\nSIGNAL: {wifi['BARS']}\nSECURITY: {wifi['SECURITY']}\n\n"
-        
-        with open(file_path, 'w') as file:
+
+        with open(file_path, "w") as file:
             file.write(data)
 
     current_conection = network.current_conection()
@@ -23,29 +23,38 @@ def menu():
     security = network.show_security()
     qr = subprocess.Popen(
         f"qr --ascii 'WIFI:T:{security};S:{current_conection[1]};P:{saved_password};;'",
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+        text=True,
     )
     output, errors = qr.communicate()
-
 
     lines = output.splitlines()
 
     # Eliminar los bordes izquierdo y derecho
-    lines = [line[1:-1] for line in lines] 
+    lines = [line[1:-1] for line in lines]
 
-    output = '\n'.join(lines)
+    output = "\n".join(lines)
     lines = output.splitlines()
 
     # Eliminar los bordes superior e inferior
     if len(lines) > 2:
-        output = '\n'.join(lines[1:-1])
+        output = "\n".join(lines[1:-1])
     else:
-        output = ''
-    with open(f"{dir_path}/{current_conection[1]}", 'a') as file:
-        file.write("\033[47m\033[30m" + output + "\033[0m")
+        output = ""
 
-    fzf = f"cd {dir_path} && fzf --header='󰤨 Networks:{" "*10}Current: {current_conection[1]}' --preview 'cat {{}}' --bind 'focus:transform-preview-label:echo {{+}}'"
-    ssids = [item['SSID'] for item in items]
+    lines = output.splitlines()
+
+    formatted_lines = ["\033[47m\033[30m" + line + "\033[0m" for line in lines]
+
+    formatted_output = "   " + "\n   ".join(formatted_lines)
+
+    with open(f"{dir_path}/{current_conection[1]}", "a") as file:
+        file.write(formatted_output)
+
+    fzf = f"cd {dir_path} && fzf --header='󰤨 Networks:{' ' * 10}Current: {current_conection[1]}' --preview 'cat {{}}' --bind 'focus:transform-preview-label:echo {{+}}'"
+    ssids = [item["SSID"] for item in items]
 
     process = subprocess.Popen(
         fzf,
@@ -53,7 +62,7 @@ def menu():
         text=True,
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
+        stderr=subprocess.PIPE,
     )
     ssids = "\n".join(ssids)
     stdout, stderr = process.communicate(input=ssids)
@@ -80,5 +89,6 @@ def menu():
                     if connect[0] == 0:
                         print(f"Conectado correctamente a {selected_item}")
                         time.sleep(1)
+
 
 menu()
