625d74d417b91bb0aec72042887b5171faf7d766
wux4an <wux4an@duck.com>
2025-06-16 14:45:40 -0500

    change floating to front

--- .config/qtile/settings/keys.py
+++ .config/qtile/settings/keys.py
@@ -164,5 +164,5 @@ for i in groups:
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position(), start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.toggle_floating()),
+    Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
